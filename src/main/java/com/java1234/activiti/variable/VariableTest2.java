85053932c902a5232eaf7a984b7235fac89ed1a6
kevenxiang <1806322519@qq.com>
2017-08-03 22:15:41 +0800

    运行时service

---
+++ src/main/java/com/java1234/activiti/variable/VariableTest2.java
@@ -0,0 +1,205 @@
+package com.java1234.activiti.variable;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.junit.Test;
+
+import com.java1234.model.Student;
+
+/**
+ * 流程变量测试
+ * @author xiang
+ *
+ */
+public class VariableTest2 {
+	
+	/**
+	 * 获取默认流程引擎实例，会自动读取activiti.cfg.xml文件
+	 */
+	private ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+	
+	/**
+	 * 部署流程定义
+	 * @param args
+	 */
+	@Test
+	public void deploy() {
+	  Deployment deployment = processEngine.getRepositoryService()  //获取部署相关Service
+		.createDeployment()   //创建部署
+		.addClasspathResource("diagrams/StudentLeaveProcess.bpmn")  //加载资源文件
+		.addClasspathResource("diagrams/StudentLeaveProcess.png")  //加载资源文件
+		.name("学生请假流程")   //流程名称
+		.deploy();   //部署
+	  System.out.println("流程部署ID:"+deployment.getId());
+	  System.out.println("流程部署Name:"+deployment.getName());
+	}
+	
+	/**
+	 * 启动流程实例
+	 */
+	@Test
+	public void start(){
+		ProcessInstance pi = processEngine.getRuntimeService()  //运行时Service
+		.startProcessInstanceByKey("studentLeaveProcess");  //流程定义表的KEY字段值
+		
+		System.out.println("流程实例ID:"+pi.getId());
+		System.out.println("流程定义ID:"+pi.getProcessDefinitionId());
+		
+ 	}
+	
+	/**
+	 * 查看任务
+	 */
+	@Test
+	public void findTask() {
+		List<Task> taskList = processEngine.getTaskService()  //任务相关Service
+		   .createTaskQuery()    //创建任务查询
+		   .taskAssignee("王五")  //指定某个人
+		   .list();
+		for(Task task:taskList) {
+			System.out.println("任务ID:"+task.getId());
+			System.out.println("任务名称:"+task.getName());
+			System.out.println("任务创建时间:"+task.getCreateTime());
+			System.out.println("任务委派人:"+task.getAssignee());
+			System.out.println("流程实例ID:"+task.getProcessInstanceId());
+		}
+	}
+	
+	
+	/**
+	 * 完成任务
+	 */
+	@Test
+	public void completeTask(){
+		processEngine.getTaskService()  //任务相关Service
+		.complete("62502");
+	}
+	
+	
+	/**
+	 * 设置流程变量数据
+	 */
+	@Test
+	public void setVariableValues() {
+		
+		RuntimeService runtimeService = processEngine.getRuntimeService();  //任务service
+		String executionId="42504";
+		runtimeService.setVariable(executionId, "days", 2);
+		runtimeService.setVariable(executionId, "date", new Date());
+		runtimeService.setVariable(executionId, "reason", "发烧");
+		
+		Student student = new Student();
+		student.setId(1);
+		student.setName("张三");
+		
+		runtimeService.setVariable(executionId, "student", student);  //支持序列化对象
+	}
+	
+	
+	/**
+	 * 获取流程变量数据
+	 */
+	@Test
+	public void getVariableValues() {
+		
+		RuntimeService runtimeService = processEngine.getRuntimeService();  //任务service
+		String executionId="50002";
+		Integer days = (Integer)runtimeService.getVariable(executionId, "days");
+		Date date = (Date)runtimeService.getVariable(executionId, "date");
+		String reason = (String)runtimeService.getVariable(executionId, "reason");
+		
+		Student student = (Student)runtimeService.getVariable(executionId, "student");  //支持序列化对象
+		
+		System.out.println("请假天数:"+days);
+		System.out.println("请假日期:"+date);
+		System.out.println("请假原因:"+reason);
+		System.out.println("请假对象:"+student.getId()+","+student.getName());
+	}
+	
+	/**
+	 * 设置流程变量数据
+	 */
+	@Test
+	public void setVariableValues2() {
+		
+		TaskService taskService = processEngine.getTaskService();  //任务service
+		String taskId="55004";
+		
+		Student student = new Student();
+		student.setId(1);
+		student.setName("张三");
+		
+		Map<String, Object> variables = new HashMap<String, Object>();
+		variables.put("days", 2);
+		variables.put("date", new Date());
+		variables.put("reason", "发烧");
+		variables.put("student", student);
+		
+		taskService.setVariables(taskId, variables);
+		
+	}
+	
+	
+	/**
+	 * 获取流程变量数据
+	 */
+	@Test
+	public void getVariableValues2() {
+		
+		TaskService taskService = processEngine.getTaskService();  //任务service
+		String taskId="60002";
+		
+		Map<String, Object> variables = taskService.getVariables(taskId);
+		
+		Integer days = (Integer)variables.get("days");
+		Date date = (Date)variables.get("date");
+		String reason = (String)variables.get("reason");
+		
+		Student student = (Student)variables.get("student");  //支持序列化对象
+		
+		System.out.println("请假天数:"+days);
+		System.out.println("请假日期:"+date);
+		System.out.println("请假原因:"+reason);
+		System.out.println("请假对象:"+student.getId()+","+student.getName());
+	}
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
